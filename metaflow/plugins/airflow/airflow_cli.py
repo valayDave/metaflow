374b6330943b149836156e0aaed8de2935ceca7f
Valay Dave <valaygaurang@gmail.com>
2022-07-14 12:43:08 -0700

    Foreach polish (valayDave/metaflow#62)
    * Removing unused imports
    * Added validation logic for airflow version numbers with foreaches
    * Removed `airflow_schedule_interval` decorator.
    
    * Added production/deployment token related changes
    - Uses s3 as a backend to store the production token
    - Token used for avoiding nameclashes
    - token stored via `FlowDatastore`
    
    * Graph type validation for airflow foreachs
    - Airflow foreachs only support single node fanout.
    - validation invalidates graphs with nested foreachs
    
    * Added configuration about startup_timeout.
    
    * Added final todo on `resources` argument of k8sOp
    - added a commented code block
    - it needs to be uncommented when airflow releasese the patch for the op
    - Code seems feature complete keeping aside airflow patch

--- metaflow/plugins/airflow/airflow_cli.py
+++ metaflow/plugins/airflow/airflow_cli.py
@@ -1,18 +1,131 @@
 import os
 import re
 import sys
-
-from metaflow import S3, current, decorators
+import base64
+from metaflow import current, decorators
 from metaflow._vendor import click
-from metaflow.exception import MetaflowException
+from metaflow.exception import MetaflowException, MetaflowInternalError
 from metaflow.package import MetaflowPackage
-from metaflow.plugins import EnvironmentDecorator, KubernetesDecorator
-from metaflow.util import get_username
+from hashlib import sha1
+from metaflow.plugins import KubernetesDecorator
+from metaflow.util import get_username, to_bytes, to_unicode
 
 from .airflow import Airflow
 from .exception import AirflowException, NotSupportedException
 
+from metaflow.plugins.aws.step_functions.production_token import (
+    load_token,
+    new_token,
+    store_token,
+)
+
+
+class IncorrectProductionToken(MetaflowException):
+    headline = "Incorrect production token"
+
+
 VALID_NAME = re.compile("[^a-zA-Z0-9_\-\.]")
+
+
+def resolve_token(
+    name, token_prefix, obj, authorize, given_token, generate_new_token, is_project
+):
+    # 1) retrieve the previous deployment, if one exists
+
+    workflow = Airflow.get_existing_deployment(name, obj.flow_datastore)
+    if workflow is None:
+        obj.echo(
+            "It seems this is the first time you are deploying *%s* to "
+            "Airflow." % name
+        )
+        prev_token = None
+    else:
+        prev_user, prev_token = workflow
+
+    # 2) authorize this deployment
+    if prev_token is not None:
+        if authorize is None:
+            authorize = load_token(token_prefix)
+        elif authorize.startswith("production:"):
+            authorize = authorize[11:]
+
+        # we allow the user who deployed the previous version to re-deploy,
+        # even if they don't have the token
+        if prev_user != get_username() and authorize != prev_token:
+            obj.echo(
+                "There is an existing version of *%s* on Airflow which was "
+                "deployed by the user *%s*." % (name, prev_user)
+            )
+            obj.echo(
+                "To deploy a new version of this flow, you need to use the same "
+                "production token that they used. "
+            )
+            obj.echo(
+                "Please reach out to them to get the token. Once you have it, call "
+                "this command:"
+            )
+            obj.echo("    airflow create --authorize MY_TOKEN", fg="green")
+            obj.echo(
+                'See "Organizing Results" at docs.metaflow.org for more information '
+                "about production tokens."
+            )
+            raise IncorrectProductionToken(
+                "Try again with the correct production token."
+            )
+
+    # 3) do we need a new token or should we use the existing token?
+    if given_token:
+        if is_project:
+            # we rely on a known prefix for @project tokens, so we can't
+            # allow the user to specify a custom token with an arbitrary prefix
+            raise MetaflowException(
+                "--new-token is not supported for @projects. Use --generate-new-token "
+                "to create a new token."
+            )
+        if given_token.startswith("production:"):
+            given_token = given_token[11:]
+        token = given_token
+        obj.echo("")
+        obj.echo("Using the given token, *%s*." % token)
+    elif prev_token is None or generate_new_token:
+        token = new_token(token_prefix, prev_token)
+        if token is None:
+            if prev_token is None:
+                raise MetaflowInternalError(
+                    "We could not generate a new token. This is unexpected. "
+                )
+            else:
+                raise MetaflowException(
+                    "--generate-new-token option is not supported after using "
+                    "--new-token. Use --new-token to make a new namespace."
+                )
+        obj.echo("")
+        obj.echo("A new production token generated.")
+        Airflow.save_deployment_token(get_username(), token, obj.flow_datastore)
+    else:
+        token = prev_token
+
+    obj.echo("")
+    obj.echo("The namespace of this production flow is")
+    obj.echo("    production:%s" % token, fg="green")
+    obj.echo(
+        "To analyze results of this production flow add this line in your notebooks:"
+    )
+    obj.echo('    namespace("production:%s")' % token, fg="green")
+    obj.echo(
+        "If you want to authorize other people to deploy new versions of this flow to "
+        "Airflow, they need to call"
+    )
+    obj.echo("    airflow create --authorize %s" % token, fg="green")
+    obj.echo("when deploying this flow to Airflow for the first time.")
+    obj.echo(
+        'See "Organizing Results" at https://docs.metaflow.org/ for more '
+        "information about production tokens."
+    )
+    obj.echo("")
+    store_token(token_prefix, token)
+
+    return token
 
 
 @click.group()
@@ -31,11 +144,32 @@ def cli():
 @click.pass_obj
 def airflow(obj, name=None):
     obj.check(obj.graph, obj.flow, obj.environment, pylint=obj.pylint)
-    obj.dag_name = resolve_dag_name(obj, name)
+    obj.dag_name, obj.token_prefix, obj.is_project = resolve_dag_name(name)
 
 
 @airflow.command(help="Compile a new version of this flow to Airflow DAG.")
 @click.argument("file", required=True)
+@click.option(
+    "--authorize",
+    default=None,
+    help="Authorize using this production token. You need this "
+    "when you are re-deploying an existing flow for the first "
+    "time. The token is cached in METAFLOW_HOME, so you only "
+    "need to specify this once.",
+)
+@click.option(
+    "--generate-new-token",
+    is_flag=True,
+    help="Generate a new production token for this flow. "
+    "This will move the production flow to a new namespace.",
+)
+@click.option(
+    "--new-token",
+    "given_token",
+    default=None,
+    help="Use the given production token for this flow. "
+    "This will move the production flow to the given namespace.",
+)
 @click.option(
     "--tag",
     "tags",
@@ -81,6 +215,9 @@ def airflow(obj, name=None):
 def create(
     obj,
     file,
+    authorize=None,
+    generate_new_token=False,
+    given_token=None,
     tags=None,
     is_paused_upon_creation=False,
     user_namespace=None,
@@ -95,9 +232,20 @@ def create(
 
     obj.echo("Compiling *%s* to Airflow DAG..." % obj.dag_name, bold=True)
 
+    token = resolve_token(
+        obj.dag_name,
+        obj.token_prefix,
+        obj,
+        authorize,
+        given_token,
+        generate_new_token,
+        obj.is_project,
+    )
+
     flow = make_flow(
         obj,
         obj.dag_name,
+        token,
         tags,
         is_paused_upon_creation,
         user_namespace,
@@ -120,6 +268,7 @@ def create(
 def make_flow(
     obj,
     dag_name,
+    production_token,
     tags,
     is_paused_upon_creation,
     namespace,
@@ -160,6 +309,7 @@ def make_flow(
         obj.environment,
         obj.event_logger,
         obj.monitor,
+        production_token,
         tags=tags,
         namespace=namespace,
         username=get_username(),
@@ -170,6 +320,42 @@ def make_flow(
         file_path=file,
         is_paused_upon_creation=is_paused_upon_creation,
     )
+
+
+def _validate_foreach_constraints(graph):
+    def traverse_graph(node, state):
+        if node.type == "foreach" and node.is_inside_foreach:
+            raise NotSupportedException(
+                "Step *%s* is a foreach step called within a foreach step. This type of graph is currently not supported with Airflow."
+                % node.name
+            )
+
+        if node.type == "foreach":
+            state["foreach_stack"] = [node.name]
+
+        if node.type in ("start", "linear", "join", "foreach"):
+            if node.type == "linear" and node.is_inside_foreach:
+                state["foreach_stack"].append(node.name)
+
+            if len(state["foreach_stack"]) > 2:
+                raise NotSupportedException(
+                    "The foreach step *%s* created by step *%s* needs to have an immidiate join step. "
+                    "Step *%s* is invalid since it is a linear step with a foreach. "
+                    "This type of graph is currently not supported with Airflow."
+                    % (
+                        state["foreach_stack"][1],
+                        state["foreach_stack"][0],
+                        state["foreach_stack"][-1],
+                    )
+                )
+
+            traverse_graph(graph[node.out_funcs[0]], state)
+
+        elif node.type == "split":
+            for func in node.out_funcs:
+                traverse_graph(graph[func], state)
+
+    traverse_graph(graph["start"], {})
 
 
 def _validate_workflow(flow, graph, flow_datastore, metadata, workflow_timeout):
@@ -184,8 +370,8 @@ def _validate_workflow(flow, graph, flow_datastore, metadata, workflow_timeout):
         )
     # check for other compute related decorators.
     # supported compute : k8s (v1), local(v2), batch(v3),
+    _validate_foreach_constraints(graph)
     for node in graph:
-        # todo : Check if there is an nesting within the foreach and throw and exception if there is a nested foreach within the code.
         if any([d.name == "batch" for d in node.decorators]):
             raise NotSupportedException(
                 "Step *%s* is marked for execution on AWS Batch with Airflow which isn't currently supported."
@@ -196,16 +382,33 @@ def _validate_workflow(flow, graph, flow_datastore, metadata, workflow_timeout):
         raise AirflowException('Datastore of type "s3" required with `airflow create`')
 
 
-def resolve_dag_name(obj, name):
+def resolve_dag_name(name):
     project = current.get("project_name")
+    is_project = False
+
     if project:
+        is_project = True
         if name:
             raise MetaflowException(
                 "--name is not supported for @projects. " "Use --branch instead."
             )
         dag_name = current.project_flow_name
+        if dag_name and VALID_NAME.search(dag_name):
+            raise MetaflowException(
+                "Name '%s' contains invalid characters. Please construct a name using regex %s"
+                % (dag_name, VALID_NAME.pattern)
+            )
+        project_branch = to_bytes(".".join((project, current.branch_name)))
+        token_prefix = (
+            "mfprj-%s"
+            % to_unicode(base64.b32encode(sha1(project_branch).digest()))[:16]
+        )
     else:
         if name and VALID_NAME.search(name):
-            raise MetaflowException("Name '%s' contains invalid characters." % name)
+            raise MetaflowException(
+                "Name '%s' contains invalid characters. Please construct a name using regex %s"
+                % (name, VALID_NAME.pattern)
+            )
         dag_name = name if name else current.flow_name
-    return dag_name
+        token_prefix = dag_name
+    return dag_name, token_prefix.lower(), is_project
